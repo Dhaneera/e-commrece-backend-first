f7771e9a233bd69dd6a3999c83906aa3d76ed829
Sehan Dhaneera Wickramsauriya <wickramsuriyashehan@gmail.com>
2024-03-26 15:42:38 +0530

    structuring api sales complete

--- Backend/ClothingManagment/src/main/java/edu/icet/clothify/service/SalesService.java
+++ Backend/ClothingManagment/src/main/java/edu/icet/clothify/service/SalesService.java
@@ -6,6 +6,6 @@ import edu.icet.clothify.entity.Sales;
 public interface SalesService {
     public Sales addSales(Sales sales);
     public boolean addSales(SalesDto salesDto);
-    public Sales updateSales(Sales sales);
+    public Sales updateSales(Long id , Sales sales);
     void deleteSales(Long id);
 }
--- Backend/ClothingManagment/src/main/java/edu/icet/clothify/service/impl/SalesServiceImpl.java
+++ Backend/ClothingManagment/src/main/java/edu/icet/clothify/service/impl/SalesServiceImpl.java
@@ -1,29 +1,45 @@
 package edu.icet.clothify.service.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.icet.clothify.config.ResourceNotFoundException;
 import edu.icet.clothify.dto.SalesDto;
 import edu.icet.clothify.entity.Sales;
+import edu.icet.clothify.repository.SalesRepository;
 import edu.icet.clothify.service.SalesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SalesServiceImpl implements SalesService {
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @Autowired
+    SalesRepository salesRepository;
     @Override
     public Sales addSales(Sales sales) {
-        return null;
+        return salesRepository.save(sales);
     }
 
     @Override
     public boolean addSales(SalesDto salesDto) {
-        return false;
+        Sales sales = mapper.convertValue(salesDto, Sales.class);
+        Sales saved = salesRepository.save(sales);
+        return saved.getId()!=null;
     }
 
     @Override
-    public Sales updateSales(Sales sales) {
-        return null;
+    public Sales updateSales(Long id ,Sales sales) {
+        if (!salesRepository.existsById(id)){
+            throw new ResourceNotFoundException("Sales not found from id : "+id);
+        }
+        sales.setId(id);
+        return salesRepository.save(sales);
     }
 
     @Override
     public void deleteSales(Long id) {
-
+        salesRepository.deleteById(id);
     }
 }
